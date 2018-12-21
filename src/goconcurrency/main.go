12467acfa4e32f9acc5f83a421f68e55cb2cfcc6
jtdv01 <jtdv01@gmail.com>
2018-12-21 17:56:46 +1100

    Cleaned up project structure

---
+++ src/goconcurrency/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    // I "goconcurrency/intro"
+    Barry "goconcurrency/barycenter"
+)
+
+func main(){
+    // I.MainGoRoutines()
+    // I.MainSharedMemory()
+
+    // Channels
+    // I.MainChannels()
+    // I.MainSingleChannel()
+    // I.MainBufferedChannels()
+
+    // I.MainNonBlocking()
+
+    Barry.NaiveBarycenterDataset()
+    // fmt.Println("hello")
+}
